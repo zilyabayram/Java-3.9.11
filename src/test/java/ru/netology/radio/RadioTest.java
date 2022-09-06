f538b86ca21918b9937f4ed82704143fa5bba153
Zilia Bairamova <ziliabairamova@gmail.com>
2022-09-06 13:04:19 +0300

    Tests has changed

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -4,31 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RadioTest {
+    Radio radio = new Radio(20);
 
     @Test
     public void shouldGetMaxStation() {
-        Radio radio = new Radio();
         radio.getMaxStation();
         System.out.println(radio.getMaxStation());
     }
 
     @Test
     public void shouldGetMinStation() {
-        Radio radio = new Radio();
         radio.getMinStation();
         System.out.println(radio.getMinStation());
     }
 
     @Test
     public void shouldGetMaxVolume() {
-        Radio radio = new Radio();
         radio.getMaxVolume();
         System.out.println(radio.getMaxVolume());
     }
 
     @Test
     public void shouldGetMinSVolume() {
-        Radio radio = new Radio();
         radio.getMinVolume();
         System.out.println(radio.getMinVolume());
     }
@@ -36,7 +33,6 @@ public class RadioTest {
 
     @Test
     public void shouldSetStation() {
-        Radio radio = new Radio();
 
         radio.setCurrentStation(15);
 
@@ -47,7 +43,6 @@ public class RadioTest {
 
     @Test
     public void shouldSetStationIfMin() {
-        Radio radio = new Radio();
 
         radio.setCurrentStation(-1);
 
@@ -58,7 +53,6 @@ public class RadioTest {
 
     @Test
     public void setNextStation() {
-        Radio radio = new Radio();
         radio.setCurrentStation(7);
 
         radio.nextStation();
@@ -72,7 +66,6 @@ public class RadioTest {
 
     @Test
     public void setNextStationIfStation9() {
-        Radio radio = new Radio();
         radio.setCurrentStation(9);
 
         radio.nextStation();
@@ -86,7 +79,6 @@ public class RadioTest {
 
     @Test
     public void setPreviousStation() {
-        Radio radio = new Radio();
         radio.setCurrentStation(1);
 
         radio.previousStation();
@@ -100,7 +92,7 @@ public class RadioTest {
 
     @Test
     public void setPreviousStationIf0() {
-        Radio radio = new Radio();
+
         radio.setCurrentStation(0);
 
         radio.previousStation();
@@ -114,7 +106,7 @@ public class RadioTest {
 
     @Test
     public void setPreviousStationIfCurrentStationMoreThan0() {
-        Radio radio = new Radio();
+
         radio.setCurrentStation(5);
 
         radio.previousStation();
@@ -128,7 +120,7 @@ public class RadioTest {
 
     @Test
     public void shouldSetVolume() {
-        Radio radio = new Radio();
+
 
         radio.setCurrentVolume(12);
 
@@ -141,7 +133,6 @@ public class RadioTest {
 
     @Test
     public void shouldSetVolumeAbove() {
-        Radio radio = new Radio();
 
         radio.setCurrentVolume(-1);
 
@@ -154,7 +145,6 @@ public class RadioTest {
 
     @Test
     public void setIncreasedVolume() {
-        Radio radio = new Radio();
         radio.setCurrentVolume(5);
 
         radio.increaseVolume();
@@ -168,7 +158,6 @@ public class RadioTest {
 
     @Test
     public void setIncreasedVolumeUp10() {
-        Radio radio = new Radio();
         radio.setCurrentVolume(10);
 
         radio.increaseVolume();
@@ -182,7 +171,7 @@ public class RadioTest {
 
     @Test
     public void setIncreasedVolumeIf10() {
-        Radio radio = new Radio();
+
         radio.setCurrentVolume(9);
 
         radio.increaseVolume();
@@ -196,7 +185,7 @@ public class RadioTest {
 
     @Test
     public void setDecreasedVolume() {
-        Radio radio = new Radio();
+
         radio.setCurrentVolume(9);
 
         radio.decreaseVolume();
@@ -210,7 +199,7 @@ public class RadioTest {
 
     @Test
     public void setDecreasedVolume0() {
-        Radio radio = new Radio();
+
         radio.setCurrentVolume(0);
 
         radio.decreaseVolume();
